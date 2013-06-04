ea51fccca39a54bea0310fedf2235dde43f51c78
Ondrej Zara <ondrej.zara@gmail.com>
2013-06-04 12:07:40 +0200

    ignore ctrl nav

--- v3/slides/keyboard/module.js
+++ v3/slides/keyboard/module.js
@@ -1,6 +1,29 @@
-Slides.addKeyListener(function(e) { Slides.prev(false); }, [8, 37, 38], "Previous slide/section");
-Slides.addKeyListener(function(e) { Slides.next(false); }, [32, 39, 40], "Next slide/section");
-Slides.addKeyListener(function(e) { Slides.prev(true); }, 33, "Previous/next slide");
-Slides.addKeyListener(function(e) { Slides.next(true); }, 34, "Previous/next slide");
-Slides.addKeyListener(function(e) { Slides.show(Slides.slides[0]); }, 36, "First/last slide");
-Slides.addKeyListener(function(e) { Slides.show(Slides.slides[Slides.slides.length-1]); }, 35, "First/last slide");
+Slides.addKeyListener(function(e) { 
+	if (e.ctrlKey) { return; }
+	Slides.prev(false);
+}, [8, 37, 38], "Previous slide/section");
+
+Slides.addKeyListener(function(e) {
+	if (e.ctrlKey) { return; }
+	Slides.next(false);
+}, [32, 39, 40], "Next slide/section");
+
+Slides.addKeyListener(function(e) {
+	if (e.ctrlKey) { return; }
+	Slides.prev(true);
+}, 33, "Previous/next slide");
+
+Slides.addKeyListener(function(e) {
+	if (e.ctrlKey) { return; }
+	Slides.next(true);
+}, 34, "Previous/next slide");
+
+Slides.addKeyListener(function(e) {
+	if (e.ctrlKey) { return; }
+	Slides.show(Slides.slides[0]);
+}, 36, "First/last slide");
+
+Slides.addKeyListener(function(e) {
+	if (e.ctrlKey) { return; }
+	Slides.show(Slides.slides[Slides.slides.length-1]);
+}, 35, "First/last slide");
