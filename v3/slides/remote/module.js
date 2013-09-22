cc4766838a2ae6bdeef3da629d0773933da0d666
Ondrej Zara <ondrej.zara@gmail.com>
2013-09-22 20:52:34 +0200

    remote simplified

--- v3/slides/remote/module.js
+++ v3/slides/remote/module.js
@@ -7,51 +7,25 @@
 	
 	var next = document.createElement("button");
 	var prev = document.createElement("button");
-	parent.appendChild(next);
-	parent.appendChild(prev);
+	next.innerHTML = "Next";
+	prev.innerHTML = "Back";
 	
 	var event = ("ontouchstart" in prev ? "touchstart" : "click");
 	next.addEventListener(event, Slides.next.bind(Slides, false));
 	prev.addEventListener(event, Slides.prev.bind(Slides, false));
 	
 	document.body.appendChild(parent);
-	
-	var format = function(before, what, a, b) {
-		return before + " " + what + " <span>(" + a + "/" + b + ")</span>";
-	}
+	parent.appendChild(next);
+	parent.appendChild(prev);
 	
 	var sync = function() {
 		var slide = Slides.current;
-		var what, a, b;
-
-		if (slide.sections.length < 2 || slide.index+1 == slide.sections.length) {
-			what = "slide";
-			a = Slides.slides.indexOf(slide) + 2;
-			b = Slides.slides.length;
-			next.disabled = (a>b);
-		} else {
-			what = "section";
-			a = slide.index+1;
-			b = slide.sections.length;
-			next.disabled = false;
-		}
-		next.innerHTML = format("Next", what, a, b);
-
-		if (slide.sections.length < 2 || slide.index == 0) {
-			what = "slide";
-			a = Slides.slides.indexOf(slide);
-			b = Slides.slides.length;
-			prev.disabled = (a < 1);
-		} else {
-			what = "section";
-			a = slide.index;
-			b = slide.sections.length;
-			prev.disabled = false;
-		}
-		prev.innerHTML = format("Previous", what, a, b);
+		var all = Slides.slides;
+		next.disabled = (all.indexOf(slide)+1 == all.length && slide.index+1 == slide.sections.length);
+		prev.disabled = (all.indexOf(slide) == 0 && slide.index < 1);
 	}
 	sync();
 	
 	Slides.addStylesheet("remote/module.css");
 	Slides.addChangeListener(sync);
-})();      
+})();
