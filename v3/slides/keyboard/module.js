becdf4ef5fd1881549dd52c4f3753c97c34dd0d6
Ondrej Zara <ondrej.zara@gmail.com>
2013-11-01 10:14:51 +0100

    preventDefault for pgup/pgdown/home/end

--- v3/slides/keyboard/module.js
+++ v3/slides/keyboard/module.js
@@ -12,20 +12,24 @@ Slides.addKeyListener(function(e) {
 
 Slides.addKeyListener(function(e) {
 	if (e.ctrlKey) { return; }
+	e.preventDefault();
 	Slides.prev(true);
 }, 33, "Previous/next slide");
 
 Slides.addKeyListener(function(e) {
 	if (e.ctrlKey) { return; }
+	e.preventDefault();
 	Slides.next(true);
 }, 34, "Previous/next slide");
 
 Slides.addKeyListener(function(e) {
 	if (e.ctrlKey) { return; }
+	e.preventDefault();
 	Slides.show(Slides.slides[0]);
 }, 36, "First/last slide");
 
 Slides.addKeyListener(function(e) {
 	if (e.ctrlKey) { return; }
+	e.preventDefault();
 	Slides.show(Slides.slides[Slides.slides.length-1]);
 }, 35, "First/last slide");
