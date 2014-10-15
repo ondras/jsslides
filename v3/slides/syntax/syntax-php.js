5d917d7a9cceb1a42db461c1947733e129f5cafd
Ondrej Zara <ondrej.zara@gmail.com>
2014-10-15 16:50:30 +0200

    php syntax

---
+++ v3/slides/syntax/syntax-php.js
@@ -0,0 +1,41 @@
+;(function(){
+	var patterns = [];
+	
+	patterns.push({
+		token: "string",
+		re: /('.*?')/g,
+		index: 1
+	});
+	
+	patterns.push({
+		token: "string",
+		re: /(".*?")/g,
+		index: 1
+	});
+	
+	patterns.push({
+		token: "number",
+		re: /([^0-9#a-z])(-?[0-9]+(\.[0-9]+)?)/ig,
+		index: 2
+	});
+
+	patterns.push({
+		token: "comment",
+		re: /(\/\*[\s\S]*?\*\/)/g,
+		index: 1
+	});
+
+	patterns.push({
+		token: "comment",
+		re: /([^:])(\/\/.*)/g,
+		index: 2
+	});
+
+	patterns.push({
+		token: "keyword",
+		re: /(\$[\w\$]+)/g,
+		index: 1
+	});
+
+	Syntax.register("php", patterns);
+})();
