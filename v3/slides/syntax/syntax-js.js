768893a4cc9c04f3f0517fc0e19eb101dced4350
Ondrej Zara <ondrej.zara@gmail.com>
2015-05-04 11:53:14 +0200

    syntax

--- v3/slides/syntax/syntax-js.js
+++ v3/slides/syntax/syntax-js.js
@@ -6,13 +6,19 @@
 		re: /('.*?')/g,
 		index: 1
 	});
-	
+
 	patterns.push({
 		token: "string",
 		re: /(".*?")/g,
 		index: 1
 	});
 	
+	patterns.push({
+		token: "string",
+		re: /(`[\s\S]*?`)/g,
+		index: 1
+	});
+
 	patterns.push({
 		token: "number",
 		re: /([^0-9#a-z])(-?[0-9]+(\.[0-9]+)?)/ig,
@@ -27,16 +33,17 @@
 
 	patterns.push({
 		token: "comment",
-		re: /([^:])(\/\/.*)/g,
+		re: /([^:]|^)(\/\/.*)/g,
 		index: 2
 	});
 
 	var keywords = ["break", "case", "catch", "continue", "default", "delete", "do", "else",
 		"finally", "for", "function", "if", "in", "instanceof", "new", "return",
 		"switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "__proto__",
-		"true", "false", "null", "NaN", "prototype", "call", "apply", "constructor"];
+		"true", "false", "null", "NaN", "prototype", "call", "apply", "constructor",
+		"let", "const", "class", "extends"];
 
-	var kw = new RegExp("(^|\\s|:|\\.|\\(|!)("+keywords.join("|")+")(?=[\\s\\.\\(\\)\\[\\];$,{}])","gm");
+	var kw = new RegExp("(^|\\s|:|\\.|\\()("+keywords.join("|")+")(?=[\\s\\.\\(\\)\\[\\];$,{}])","gm");
 	patterns.push({
 		token: "keyword",
 		re: kw,
