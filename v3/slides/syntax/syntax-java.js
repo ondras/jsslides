88298a59bdbd1803d0f1e5e2d87f4dce6697cb77
Petr Janeček <JanecekPetr@seznam.cz>
2015-10-24 22:36:53 +0200

    Update syntax-java.js
    Formatting. I'm too tired.

--- v3/slides/syntax/syntax-java.js
+++ v3/slides/syntax/syntax-java.js
@@ -7,7 +7,7 @@
 		index: 1
 	});
 	
-    patterns.push({
+	patterns.push({
 		token: "string",
 		re: /('.')/g,
 		index: 1
@@ -32,12 +32,12 @@
 	});
 
 	var keywords = ["abstract", "assert", "boolean", "break", "byte", "case",
-	"catch", "char", "class", "const", "continue", "default", "do", "double",
-	"else", "enum", "extends", "final", "finally", "float", "for", "goto",
-	"if", "implements", "import", "instanceof", "int", "interface", "long",
-	"native", "new", "package", "private", "protected", "public", "return",
-	"short", "static", "strictfp", "super", "switch", "synchronized", "this",
-	"throw", "throws", "transient", "try", "void", "volatile", "while"];
+		"catch", "char", "class", "const", "continue", "default", "do", "double",
+		"else", "enum", "extends", "final", "finally", "float", "for", "goto",
+		"if", "implements", "import", "instanceof", "int", "interface", "long",
+		"native", "new", "package", "private", "protected", "public", "return",
+		"short", "static", "strictfp", "super", "switch", "synchronized", "this",
+		"throw", "throws", "transient", "try", "void", "volatile", "while"];
 
 	var kw = new RegExp("(^|\\s|\\.|\\()("+keywords.join("|")+")(?=\\s|\\.|\\(|\\)|\\[|\\]|;|$|,)","gm");
 	patterns.push({
